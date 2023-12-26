efa82e48bf78979d4c0abecc6357f968e0936c82
Mikhail Podolskiy <134136109+mikhailpodolskiy@users.noreply.github.com>
2023-12-26 15:41:50 +0300

    Update 001 parser001.py

--- parser001.py
+++ parser001.py
@@ -1,1 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import pandas as pd
 
+data = []
+
+for p in range(1, 2213):
+
+    url = f"https://makitatrading.ru/brands/makita/?PAGEN_2={p}"
+    r = requests.get(url)
+    sleep(3)
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    tovars = soup.findAll('div', class_='itemCard R2D2')
+
+    for tovar in tovars:
+        name = tovar.find('span', class_='name').text
+        link = 'https://makitatrading.ru' + tovar.find('a', class_='item_title title').get('href')
+        price = tovar.find('span', class_='price').get_text('|', strip=True).replace(' ', '')[:-1]
+        try:
+            availability = tovar.find('div', class_='availability z7_notavail').text
+        except:
+            availability = "NUll"
+        data.append([name, link, price, availability])
+
+print("End")
+header = ['name', 'link', 'price', 'availability']
+df = pd.DataFrame(data, columns=header)
+df.to_csv('/Users/Mikhail/Downloads/makitatrading_prices.csv', sep=';', encoding='utf8')
